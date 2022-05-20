004b9d5cbb6733c125e85d870bb659cb2d69be8d
Nick Johnson <nickcjohnson84@outlook.com>
2022-05-20 14:09:31 -0600

    created logo and starting Login/Signup

--- stump-me/client/src/pages/Login.js
+++ stump-me/client/src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-// import { useMutation } from '@apollo/client';
-// import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Login = (props) => {
