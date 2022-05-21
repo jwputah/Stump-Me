8e6d5a00d3ebc2003d63ac0c8fdf79bcae6c6cb9
Nick Johnson <nickcjohnson84@outlook.com>
2022-05-20 18:05:04 -0600

    add start to Profile page and NoMatch

--- stump-me/client/src/App.js
+++ stump-me/client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './stumpMe.png';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome to Stump Me</h1>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
