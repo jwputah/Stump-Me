1d14807c340c735a6fbcdb65cb184915549be518
tyarchy <ty.d.gregory@gmail.com>
2022-05-21 21:26:55 -0600

    the navbar nick doesn't want

--- stump-me/client/src/App.js
+++ stump-me/client/src/App.js
@@ -1,9 +1,11 @@
 import logo from './stumpMe.png';
-// import './App.css';
+import Nav from './components/Nav';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav></Nav>
       <header className="App-header">
         <h1>Welcome to Stump Me</h1>
         <img src={logo} className="App-logo" alt="logo" />
---
+++ stump-me/client/src/components/Nav/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+
+
+function Nav() {
+
+    // function categorySelected(name) {
+    //     console.log(`${name} clicked`)
+    // }
+
+    // const categories = [
+
+
+    // ];
+
+    return (
+        <div class="topnav">
+            <a href="#newgame">NEW GAME</a>
+            <a href="#friends">FRIENDS</a>
+            <a href="#signout">SIGNOUT</a>
+        </div>
+    );
+}
+
+export default Nav;
