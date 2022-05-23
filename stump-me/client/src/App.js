046f184359f5a7f7c500ab077929882f740e5ed6
Chandler Beck <beckonator25@gmail.com>
2022-05-22 19:29:32 -0600

    updated App.js and added pages

--- stump-me/client/src/App.js
+++ stump-me/client/src/App.js
@@ -1,14 +1,76 @@
-import logo from './stumpMe.png';
-// import './App.css';
+import logo from "./assets/images/stumpMe.png";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Welcome to Stump Me</h1>
+//         <img src={logo} className="App-logo" alt="logo" />
+//       </header>
+//     </div>
+//   );
+// }
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// import Home from ".pages/Home";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+// import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem("id_token");
+
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : "",
+//     },
+//   };
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Stump Me</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    // <ApolloProvider client={client}>
+    <Router>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile" element={<Profile />} />
+            {/* <Route path="/thought/:id" element={<SingleThought />} /> */}
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+    // </ApolloProvider>
   );
 }
 
